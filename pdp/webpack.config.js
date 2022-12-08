43cf1e0d1823d2df4f02790e26750fa2922a5fc5
kusumadoni77 <kusumadoni77@gmail.com>
2022-12-08 19:06:16 +0700

    Update webpack.config.js

--- pdp/webpack.config.js
+++ pdp/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: "https://mf-dump-pdp.vercell.app/",
   },
 
   resolve: {
@@ -44,10 +44,10 @@ module.exports = {
       name: "pdp",
       filename: "remoteEntry.js",
       remotes: {
-        home: "home@http://localhost:3000/remoteEntry.js",
-        pdp: "pdp@http://localhost:3001/remoteEntry.js",
-        cart: "cart@http://localhost:3002/remoteEntry.js",
-        addtocart: "addtocart@http://localhost:3003/remoteEntry.js",
+        home: "home@https://microfrontend-sage.vercel.app/remoteEntry.js",
+        pdp: "pdp@https://mf-dump-pdp.vercell.app/remoteEntry.js",
+        cart: "cart@https://cart-mf.vercell.app/remoteEntry.js",
+        addtocart: "addtocart@https://addtocart-mf.vercell.app/remoteEntry.js",
       },
       exposes: {
         "./PDPContent": "./src/PDPContent.jsx",
